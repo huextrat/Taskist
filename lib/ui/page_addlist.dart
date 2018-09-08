acc81f11707b7bd0cf6ff0a6abd0569b286993b0
Hugo EXTRAT <extrat.h@gmail.com>
2018-09-08 12:23:08 +0200

    Check connectivity

--- lib/ui/page_addlist.dart
+++ lib/ui/page_addlist.dart
@@ -1,8 +1,13 @@
+import 'dart:async';
+import 'package:flutter/services.dart';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
+
+import 'package:connectivity/connectivity.dart';
 
 class NewTaskPage extends StatefulWidget {
   final FirebaseUser user;
@@ -24,195 +29,235 @@ class _NewTaskPageState extends State<NewTaskPage> {
 
   bool _saving = false;
 
-  void addToFirebase() async {
+  String _connectionStatus = 'Unknown';
+  final Connectivity _connectivity = new Connectivity();
+  StreamSubscription<ConnectivityResult> _connectivitySubscription;
 
+  Future<Null> initConnectivity() async {
+    String connectionStatus;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      connectionStatus = (await _connectivity.checkConnectivity()).toString();
+    } on PlatformException catch (e) {
+      print(e.toString());
+      connectionStatus = 'Failed to get connectivity.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) {
+      return;
+    }
+
+    setState(() {
+      _connectionStatus = connectionStatus;
+    });
+  }
+
+  void addToFirebase() async {
     setState(() {
       _saving = true;
     });
 
-    bool isExist = false;
+    print(_connectionStatus);
 
-    QuerySnapshot query =
-        await Firestore.instance.collection(widget.user.uid).getDocuments();
+    if(_connectionStatus == "ConnectivityResult.none"){
+      showInSnackBar("No internet connection currently available");
+      setState(() {
+        _saving = false;
+      });
+    } else {
 
-    query.documents.forEach((doc) {
-      if (listNameController.text.toString() == doc.documentID) {
-        isExist = true;
-      }
-    });
+      bool isExist = false;
 
-    if (isExist == false && listNameController.text.isNotEmpty) {
-      await Firestore.instance
-          .collection(widget.user.uid)
-          .document(listNameController.text.toString().trim())
-          .setData({
-        "color": currentColor.value.toString(),
-        "date": DateTime.now().millisecondsSinceEpoch
+      QuerySnapshot query =
+      await Firestore.instance.collection(widget.user.uid).getDocuments();
+
+      query.documents.forEach((doc) {
+        if (listNameController.text.toString() == doc.documentID) {
+          isExist = true;
+        }
       });
 
-      listNameController.clear();
+      if (isExist == false && listNameController.text.isNotEmpty) {
+        await Firestore.instance
+            .collection(widget.user.uid)
+            .document(listNameController.text.toString().trim())
+            .setData({
+          "color": currentColor.value.toString(),
+          "date": DateTime.now().millisecondsSinceEpoch
+        });
 
-      pickerColor = Color(0xff6633ff);
-      currentColor = Color(0xff6633ff);
+        listNameController.clear();
 
-      Navigator.of(context).pop();
-    }
-    if (isExist == true) {
-      showInSnackBar("This list already exists");
-    }
-    if (listNameController.text.isEmpty) {
-      showInSnackBar("Please enter a name");
+        pickerColor = Color(0xff6633ff);
+        currentColor = Color(0xff6633ff);
+
+        Navigator.of(context).pop();
+      }
+      if (isExist == true) {
+        showInSnackBar("This list already exists");
+        setState(() {
+          _saving = false;
+        });
+      }
+      if (listNameController.text.isEmpty) {
+        showInSnackBar("Please enter a name");
+        setState(() {
+          _saving = false;
+        });
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        key: _scaffoldKey,
-        body: ModalProgressHUD(
-            child: new Stack(
-              children: <Widget>[
-                _getToolbar(context),
-                Container(
-                  child: Column(
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.only(top: 100.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: <Widget>[
-                            Expanded(
-                              flex: 1,
-                              child: Container(
-                                color: Colors.grey,
-                                height: 1.5,
-                              ),
+      key: _scaffoldKey,
+      body: ModalProgressHUD(
+          child: new Stack(
+            children: <Widget>[
+              _getToolbar(context),
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(top: 100.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                              color: Colors.grey,
+                              height: 1.5,
                             ),
-                            Expanded(
-                                flex: 2,
-                                child: new Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: <Widget>[
-                                    Text(
-                                      'New',
-                                      style: new TextStyle(
-                                          fontSize: 30.0,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                    Text(
-                                      'List',
-                                      style: new TextStyle(
-                                          fontSize: 28.0, color: Colors.grey),
-                                    )
-                                  ],
-                                )),
-                            Expanded(
-                              flex: 1,
-                              child: Container(
-                                color: Colors.grey,
-                                height: 1.5,
-                              ),
+                          ),
+                          Expanded(
+                              flex: 2,
+                              child: new Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text(
+                                    'New',
+                                    style: new TextStyle(
+                                        fontSize: 30.0,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    'List',
+                                    style: new TextStyle(
+                                        fontSize: 28.0, color: Colors.grey),
+                                  )
+                                ],
+                              )),
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                              color: Colors.grey,
+                              height: 1.5,
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
-                      Padding(
-                        padding:
-                        EdgeInsets.only(top: 50.0, left: 20.0, right: 20.0),
-                        child: new Column(
-                          children: <Widget>[
-                            new TextFormField(
-                              decoration: InputDecoration(
-                                  border: new OutlineInputBorder(
-                                      borderSide:
-                                      new BorderSide(color: Colors.teal)),
-                                  labelText: "List name",
-                                  contentPadding: EdgeInsets.only(
-                                      left: 16.0,
-                                      top: 20.0,
-                                      right: 16.0,
-                                      bottom: 5.0)),
-                              controller: listNameController,
-                              autofocus: true,
-                              style: TextStyle(
-                                fontSize: 22.0,
-                                color: Colors.black,
-                                fontWeight: FontWeight.w500,
-                              ),
-                              keyboardType: TextInputType.text,
-                              textCapitalization: TextCapitalization.sentences,
-                              maxLength: 20,
+                    ),
+                    Padding(
+                      padding:
+                          EdgeInsets.only(top: 50.0, left: 20.0, right: 20.0),
+                      child: new Column(
+                        children: <Widget>[
+                          new TextFormField(
+                            decoration: InputDecoration(
+                                border: new OutlineInputBorder(
+                                    borderSide:
+                                        new BorderSide(color: Colors.teal)),
+                                labelText: "List name",
+                                contentPadding: EdgeInsets.only(
+                                    left: 16.0,
+                                    top: 20.0,
+                                    right: 16.0,
+                                    bottom: 5.0)),
+                            controller: listNameController,
+                            autofocus: true,
+                            style: TextStyle(
+                              fontSize: 22.0,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w500,
                             ),
-                            new Padding(
-                              padding: EdgeInsets.only(bottom: 10.0),
-                            ),
-                            ButtonTheme(
-                              minWidth: double.infinity,
-                              child: RaisedButton(
-                                elevation: 3.0,
-                                onPressed: () {
-                                  pickerColor = currentColor;
-                                  showDialog(
-                                    context: context,
-                                    builder: (BuildContext context) {
-                                      return AlertDialog(
-                                        title: const Text('Pick a color!'),
-                                        content: SingleChildScrollView(
-                                          child: ColorPicker(
-                                            pickerColor: pickerColor,
-                                            onColorChanged: changeColor,
-                                            enableLabel: true,
-                                            colorPickerWidth: 1000.0,
-                                            pickerAreaHeightPercent: 0.7,
-                                          ),
+                            keyboardType: TextInputType.text,
+                            textCapitalization: TextCapitalization.sentences,
+                            maxLength: 20,
+                          ),
+                          new Padding(
+                            padding: EdgeInsets.only(bottom: 10.0),
+                          ),
+                          ButtonTheme(
+                            minWidth: double.infinity,
+                            child: RaisedButton(
+                              elevation: 3.0,
+                              onPressed: () {
+                                pickerColor = currentColor;
+                                showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return AlertDialog(
+                                      title: const Text('Pick a color!'),
+                                      content: SingleChildScrollView(
+                                        child: ColorPicker(
+                                          pickerColor: pickerColor,
+                                          onColorChanged: changeColor,
+                                          enableLabel: true,
+                                          colorPickerWidth: 1000.0,
+                                          pickerAreaHeightPercent: 0.7,
                                         ),
-                                        actions: <Widget>[
-                                          FlatButton(
-                                            child: Text('Got it'),
-                                            onPressed: () {
-                                              setState(
-                                                      () => currentColor = pickerColor);
-                                              Navigator.of(context).pop();
-                                            },
-                                          ),
-                                        ],
-                                      );
-                                    },
-                                  );
-                                },
-                                child: Text('Card color'),
-                                color: currentColor,
-                                textColor: const Color(0xffffffff),
-                              ),
+                                      ),
+                                      actions: <Widget>[
+                                        FlatButton(
+                                          child: Text('Got it'),
+                                          onPressed: () {
+                                            setState(() =>
+                                                currentColor = pickerColor);
+                                            Navigator.of(context).pop();
+                                          },
+                                        ),
+                                      ],
+                                    );
+                                  },
+                                );
+                              },
+                              child: Text('Card color'),
+                              color: currentColor,
+                              textColor: const Color(0xffffffff),
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 50.0),
-                        child: new Column(
-                          children: <Widget>[
-                            new RaisedButton(
-                              child: const Text(
-                                'Add',
-                                style: TextStyle(color: Colors.white),
-                              ),
-                              color: Colors.blue,
-                              elevation: 4.0,
-                              splashColor: Colors.deepPurple,
-                              onPressed: addToFirebase,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 50.0),
+                      child: new Column(
+                        children: <Widget>[
+                          new RaisedButton(
+                            child: const Text(
+                              'Add',
+                              style: TextStyle(color: Colors.white),
                             ),
-                          ],
-                        ),
+                            color: Colors.blue,
+                            elevation: 4.0,
+                            splashColor: Colors.deepPurple,
+                            onPressed: addToFirebase,
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            inAsyncCall: _saving
-        ),
+              ),
+            ],
+          ),
+          inAsyncCall: _saving),
     );
   }
 
@@ -223,12 +268,20 @@ class _NewTaskPageState extends State<NewTaskPage> {
   @override
   void dispose() {
     _scaffoldKey.currentState?.dispose();
+    _connectivitySubscription?.cancel();
     super.dispose();
   }
 
   @override
   void initState() {
     super.initState();
+    initConnectivity();
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen((ConnectivityResult result) {
+          setState(() {
+            _connectionStatus = result.toString();
+          });
+        });
   }
 
   void showInSnackBar(String value) {
